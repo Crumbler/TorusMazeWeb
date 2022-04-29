fbfdd5b5c76a3df109e4f745168d159b45d94d55
Crumbler <25861574+Crumbler@users.noreply.github.com>
2022-04-29 13:27:12 +0300

    Use ShaderProgram

--- js/main.js
+++ js/main.js
@@ -9,38 +9,6 @@ const canvas = Global.canvas;
 const gl = Global.gl;
 
 
-function createShader(gl, type, source) {
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-    if (success) {
-      return shader;
-    }
-  
-    console.log(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-
-    return undefined;
-  }
-  
-  
-  function createProgram(gl, vertexShader, fragmentShader) {
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-      return program;
-    }
-  
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
-    return undefined;
-}
-
-
 async function main() {
     if (!gl) {
         console.error('WebGL 2 not supported');
@@ -51,26 +19,25 @@ async function main() {
 
     gl.viewport(0, 0, canvas.width, canvas.height);
 
+    gl.clearColor(0, 0, 0, 0);
+
     await Global.Init();
 
-    var vertexShader = createShader(gl, gl.VERTEX_SHADER, Global.shaders.vShader);
-    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, Global.shaders.fShader);
-
-    // Link the two shaders into a program
-    var program = createProgram(gl, vertexShader, fragmentShader);
+    const program = new ShaderProgram(new ShaderInfo('Vertex shader', Global.shaders.vShader, gl.VERTEX_SHADER),
+      new ShaderInfo('Fragment shader', Global.shaders.fShader, gl.FRAGMENT_SHADER));
 
     const positions = [
       -1, 1,
       1, 1,
-      1, -1,
+      -1, -1,
+      1, -1
     ];
 
     const model = Loader.LoadTorusRect(positions);
 
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    Renderer.Prepare();
 
-    gl.useProgram(program);
+    program.start();
 
     Renderer.RenderTorusRect(model);
 }
